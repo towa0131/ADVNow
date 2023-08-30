af28bcf2fe5b0a995389657bf0a7265989cd4572
towa0131 <towa.towa0131@gmail.com>
2023-08-30 10:48:43 +0900

    [Add] 実行ファイルの存在チェックを追加

--- ADVNow/Commands/LaunchGameCommand.cs
+++ ADVNow/Commands/LaunchGameCommand.cs
@@ -54,78 +54,90 @@ namespace ADVNow.Commands
             Game game = this._vm.CurrentGame.Value;
             if (game != null)
             {
-                try
+                if (File.Exists(game.Path))
                 {
-                    Task launchTask = new Task(async () =>
+                    try
                     {
-                        string path = game.Path;
-                        string imagePath = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\ADVNow\\tmp.png";
-                        ProcessStartInfo pinfo = new ProcessStartInfo();
-                        pinfo.FileName = path;
-                        Process? p = Process.Start(pinfo);
-                        Bitmap? icon = Icon.ExtractAssociatedIcon(path)?.ToBitmap() ?? null;
-                        icon?.Save(imagePath, System.Drawing.Imaging.ImageFormat.Png);
-                        _rpcClient = new DiscordRpcClient(this._token);
-                        this._rpcClient.Initialize();
-                        Timer timer = new Timer(1000);
-                        this._startTime = DateTime.UtcNow;
-                        int totalSec = 0;
-                        timer.Elapsed += async (sender, args) => {
-                            if (this._isShowing != this._vm.DiscordStatus.Value)
+                        Task launchTask = new Task(async () =>
+                        {
+                            string path = game.Path;
+                            string imagePath = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\ADVNow\\tmp.png";
+                            ProcessStartInfo pinfo = new ProcessStartInfo();
+                            pinfo.FileName = path;
+                            Process? p = Process.Start(pinfo);
+                            Bitmap? icon = Icon.ExtractAssociatedIcon(path)?.ToBitmap() ?? null;
+                            icon?.Save(imagePath, System.Drawing.Imaging.ImageFormat.Png);
+                            _rpcClient = new DiscordRpcClient(this._token);
+                            this._rpcClient.Initialize();
+                            Timer timer = new Timer(1000);
+                            this._startTime = DateTime.UtcNow;
+                            int totalSec = 0;
+                            timer.Elapsed += async (sender, args) => {
+                                if (this._isShowing != this._vm.DiscordStatus.Value)
+                                {
+                                    this._isShowing = this._vm.DiscordStatus.Value;
+                                    if (this._vm.DiscordStatus.Value)
+                                    {
+                                        this.SetPresence(game);
+                                    }
+                                    else
+                                    {
+                                        this._rpcClient.ClearPresence();
+                                    }
+                                }
+                                if (this._isShowing) this._rpcClient.Invoke();
+                                totalSec++;
+                                int hour = totalSec / 3600;
+                                int min = (totalSec % 3600) / 60;
+                                int sec = totalSec % 60;
+                                this._vm.PlayingTimeString.Value = String.Format("{0:D2}:{1:D2}:{2:D2}", hour, min, sec);
+                            };
+
+                            using (FileStream fileStream = File.OpenRead(imagePath))
                             {
-                                this._isShowing = this._vm.DiscordStatus.Value;
-                                if (this._vm.DiscordStatus.Value)
+                                using (HttpClient httpClient = new HttpClient())
                                 {
-                                    this.SetPresence(game);
-                                } else
-                                {
-                                    this._rpcClient.ClearPresence();
+                                    ImageEndpoint imageEndpoint = new ImageEndpoint(this._imgurClient, httpClient);
+                                    IImage imageUpload = await imageEndpoint.UploadImageAsync(fileStream);
+                                    this._imageUrl = imageUpload.Link;
                                 }
                             }
-                            if (this._isShowing) this._rpcClient.Invoke();
-                            totalSec++;
-                            int hour = totalSec / 3600;
-                            int min = (totalSec % 3600) / 60;
-                            int sec = totalSec % 60;
-                            this._vm.PlayingTimeString.Value = String.Format("{0:D2}:{1:D2}:{2:D2}", hour, min, sec);
-                        };
 
-                        using (FileStream fileStream = File.OpenRead(imagePath))
-                        {
-                            using (HttpClient httpClient = new HttpClient())
+                            if (this._isShowing) this.SetPresence(game);
+
+                            this._vm.PlayingGameString.Value = game.Title + " をプレイ中";
+                            this._vm.PlayingTimeString.Value = "00:00:00";
+                            timer.Start();
+                            p?.WaitForExit();
+                            timer.EndInit();
+                            this._rpcClient.Dispose();
+                            game.TotalPlayMinutes += totalSec / 60;
+                            game.LastPlay = DateTime.Now;
+                            for (int i = 0; i < this._vm.AllGames.Count(); i++)
                             {
-                                ImageEndpoint imageEndpoint = new ImageEndpoint(this._imgurClient, httpClient);
-                                IImage imageUpload = await imageEndpoint.UploadImageAsync(fileStream);
-                                this._imageUrl = imageUpload.Link;
+                                if (this._vm.AllGames[i].Title == game.Title)
+                                {
+                                    this._vm.AllGames[i] = game;
+                                    break;
+                                }
                             }
-                        }
-
-                        if (this._isShowing) this.SetPresence(game);
-
-                        this._vm.PlayingGameString.Value = game.Title + " をプレイ中";
-                        this._vm.PlayingTimeString.Value = "00:00:00";
-                        timer.Start();
-                        p?.WaitForExit();
-                        timer.EndInit();
-                        this._rpcClient.Dispose();
-                        game.TotalPlayMinutes += totalSec / 60;
-                        game.LastPlay = DateTime.Now;
-                        for (int i = 0; i < this._vm.AllGames.Count(); i++)
-                        {
-                            if (this._vm.AllGames[i].Title == game.Title)
-                            {
-                                this._vm.AllGames[i] = game;
-                                break;
-                            }
-                        }
-                        this._vm.PlayingGameString.Value = "---";
-                        this._vm.PlayingTimeString.Value = "";
-                    });
-                    launchTask.Start();
+                            this._vm.PlayingGameString.Value = "---";
+                            this._vm.PlayingTimeString.Value = "";
+                        });
+                        launchTask.Start();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("ゲームを起動することができませんでした。\n" + ex.Message);
+                    }
                 }
-                catch (Exception ex)
+                else
                 {
-                    MessageBox.Show("ゲームを起動することができませんでした。\n" +  ex.Message);
+                    if (MessageBox.Show("実行ファイルが見つかりません。\n" + game.Title + " を削除しますか？", "", MessageBoxButton.YesNo,
+                    MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                    {
+                        this._vm.AllGames.Remove(game);
+                    }
                 }
             }
         }
